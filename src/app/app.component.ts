006ed354d351e7dc114a0d6cbc88b35010773d57
Tanpisit Jampa <nakimaaot@gmail.com>
2017-02-13 12:38:07 +0700

    Completed workshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,10 @@
-import {HeroService} from "./services/hero.service";
-import { HEROES } from "./model/mock-heroes";
-import { Hero } from "./model/hero";
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of heroes';
-  heroes: Hero[] = HEROES;
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) {}
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  public getHeroes(): any {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Hero } from "../model/hero";
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { Hero } from "../model/hero";
+import { HeroService } from "../services/hero.service";
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,9 +16,24 @@ export class HeroDetailComponent implements OnInit {
   @Input()
   hero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.route.params.switchMap((params: Params) =>
+      this.heroService.getHero(+params['id']))
+        .subscribe(hero => this.hero = hero)
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+        .then(() => this.goBack());
+  }
 }
